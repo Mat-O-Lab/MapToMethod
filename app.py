ec3b1d42d98c53333dfae366f92854c2d9b20d8f
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-03-24 12:13:48 +0100

    version number increase

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     app_name: str = "MaptoMethod"
     admin_email: str = os.environ.get("ADMIN_MAIL") or "csvtocsvw@matolab.org"
     items_per_user: int = 50
-    version: str = "v1.0.5"
+    version: str = "v1.0.6"
     config_name: str = os.environ.get("APP_MODE") or "development"
     openapi_url: str ="/api/openapi.json"
     docs_url: str = "/api/docs"
